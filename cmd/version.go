952a417544d5a3b4640c8055f636fc539507645c
Rodrigo Moran <rigo.moran0@gmail.com>
2024-07-26 09:49:06 -0300

    refactor version method

--- cmd/version.go
+++ cmd/version.go
@@ -13,32 +13,21 @@ var (
 	short   = false
 )
 
-var versionUsage = `Print the app version and build info for the current context.
-
-Usage: gupi version [options]
-
-Options:
-  --short  If true, print just the version number. Default false.
-`
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the app version and build info of gupi",
+	Long:  "Print the app version and build info for the current context",
+	Run: func(cmd *cobra.Command, args []string) {
+		if short {
+			fmt.Printf("gupi version: v%s", version)
+		} else {
+			fmt.Printf("gupi version: v%s, build: %s", version, build)
+		}
+		os.Exit(0)
+	},
+}
 
 func init() {
 	versionCmd.Flags().BoolVarP(&short, "short", "s", short, "")
 	rootCmd.AddCommand(versionCmd)
 }
-
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Print the app version and build info of gupi",
-	Long:  versionUsage,
-	Run:   versionFunc,
-}
-
-var versionFunc = func(cmd *cobra.Command, args []string) {
-	if short {
-		fmt.Printf("gupi version: v%s", version)
-	} else {
-		fmt.Printf("gupi version: v%s, build: %s", version, build)
-	}
-	os.Exit(0)
-}
-
