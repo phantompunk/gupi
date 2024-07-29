fd476fbc4d99a0ddcf420ce1899825c1e0045cb1
Rodrigo Moran <rigo.moran0@gmail.com>
2024-07-28 22:52:46 -0300

    add list method

--- gupi/editor.go
+++ gupi/editor.go
@@ -46,6 +46,21 @@ func DeleteTemplate(tmplName string) error {
 	return nil
 }
 
+func ListTemplates() error {
+	fmt.Fprint(os.Stdout, "testing\n")
+	filelist, err := fs.ReadTemplates()
+	if err != nil {
+		return errors.New("Unable to read templates")
+	}
+
+	fmt.Printf("NAME\t\tSIZE\t\tMODIFIED")
+	for _, files := range filelist {
+		fmt.Printf("\n%-15s %-15v %v\n", files.Name(), files.Size(), files.ModTime().Format("2006-01-02 15:04:05"))
+	}
+
+	return nil
+}
+
 func RenderTemplate(fileName, tmplName string) error {
 	path, err := fs.GetTemplatePath(tmplName)
 	if err != nil {
