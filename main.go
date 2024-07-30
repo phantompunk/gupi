706c2d42f55fb1809e92808158d81c026f75a07b
Rodrigo Moran <rigo.moran0@gmail.com>
2024-07-30 14:37:43 -0300

    remove commented code

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"github.com/phantompunk/gupi/cmd"
-	store "github.com/phantompunk/gupi/internal"
-	"github.com/spf13/afero"
 )
 
-var editor *store.Editor
-
 func main() {
-	fileSystem := store.NewFileStore("$HOME/.gupi/templates", afero.NewOsFs())
-	editor = store.NewEditor(fileSystem)
 	cmd.Execute()
 }
